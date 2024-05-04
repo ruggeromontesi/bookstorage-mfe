f7018ca4e151e7396786420b1f71f829ead8495d
Ruggero Montesi <ruggero.montesi1@gmail.com>
2024-05-04 10:38:07 +0300

    add support for unit price

--- components/BookForm.js
+++ components/BookForm.js
@@ -23,13 +23,17 @@ const bookComponent = {
         </label>
         
         <label for="barcode">
-            Barcode
+            Barcode:
             <input id = "barcode" v-model="barcode"></input>
         </label>
-        <label for="quantity">
+        <label for="quantity" >
             Quantity:
-            <input id="quantity" v-model="quantity"></input>
+            <input id="quantity" v-model="quantity" class="quantity"></input>
         </label>
+        <label for="price" >
+            Price:
+        <input id="price" v-model="price" class="price"></input>
+    </label>
         <br>
         <button class="b" v-on:click="onSubmit">Submit</button> 
     </div>
@@ -40,7 +44,8 @@ const bookComponent = {
             title: 'Ocp Oracle Certified Professional Java Se 11 Developer Complete Study Guide',
             author: 'Jeanne Boyarsky Scott Selikoff',
             barcode: generateUpc(),
-            quantity: 0
+            quantity: 0,
+            price: 1.05,
         }
     },
 
@@ -51,12 +56,15 @@ const bookComponent = {
                 author: this.author,
                 barcode:  this.barcode,
                 quantity: this.quantity,
+                price: this.price,
             }
 
             this.$emit('book-created', book)
             this.title = '',
             this.author = '',
-            this.quantity = 1
+            this.quantity = 1,
+            this.barcode = generateUpc(),
+            this.price = 1.05 
 
         }
     }
